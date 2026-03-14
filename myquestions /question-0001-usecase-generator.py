f3cab86ae110ea66645a82bde297d05e1ea47f4e
BryanM-D <143416213+BryanM-D@users.noreply.github.com>
2026-03-14 11:14:19 -0500

    Update question-0001-usecase-generator.py

--- myquestions /question-0001-usecase-generator.py
+++ myquestions /question-0001-usecase-generator.py
@@ -1,31 +1,23 @@
 import numpy as np
 import pandas as pd
 
-def generar_datos_sensores(n=500, seed=42):
+def generar_creditos(n=1000, seed=42):
     np.random.seed(seed)
     
-    temperatura = np.random.normal(75, 10, n)
-    vibracion = np.random.normal(5, 2, n)
-    presion = np.random.normal(200, 20, n)
-    ruido = np.random.normal(80, 5, n)
-    horas_operacion = np.random.randint(0, 10000, n)
+    edad = np.random.randint(18, 65, n)
+    ingresos = np.random.normal(3000, 1000, n)
+    puntaje = np.random.normal(600, 100, n)
+    genero = np.random.randint(0, 2, n)
     
-    # Generamos probabilidad de falla basada en condiciones extremas
-    prob_falla = (
-        (temperatura > 90).astype(int) +
-        (vibracion > 8).astype(int) +
-        (presion > 240).astype(int)
-    )
-    
-    estado_falla = (prob_falla > 1).astype(int)
+    prob = (puntaje/850) + (ingresos/10000)
+    aprobado = (prob > 0.9).astype(int)
     
     df = pd.DataFrame({
-        "temperatura": temperatura,
-        "vibracion": vibracion,
-        "presion": presion,
-        "ruido": ruido,
-        "horas_operacion": horas_operacion,
-        "estado_falla": estado_falla
+        "edad": edad,
+        "ingresos": ingresos,
+        "puntaje_credito": puntaje,
+        "genero": genero,
+        "aprobado": aprobado
     })
     
     return df
